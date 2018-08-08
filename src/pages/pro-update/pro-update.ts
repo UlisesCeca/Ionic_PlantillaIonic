808db9f35c7b3f707a6b915014bb75512d9684a8
UlisesCeca <ulises@ulisesceca.com>
2018-08-08 19:33:19 +0200

    creada pagina y provider pro

--- src/pages/pro-update/pro-update.ts
+++ src/pages/pro-update/pro-update.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the ProUpdatePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { ProUpdateProvider } from '../../providers/pro-update/pro-update'
 
 @IonicPage()
 @Component({
@@ -15,7 +9,10 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ProUpdatePage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(private navCtrl: NavController,
+              private navParams: NavParams,
+              public pro: ProUpdateProvider
+            ) {
   }
 
   ionViewDidLoad() {
--- src/providers/pro-update/pro-update.ts
+++ src/providers/pro-update/pro-update.ts
@@ -1,17 +1,25 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Pro } from '../../../node_modules/@ionic/pro';
 
-/*
-  Generated class for the ProUpdateProvider provider.
-
-  See https://angular.io/guide/dependency-injection for more info on providers
-  and Angular DI.
-*/
 @Injectable()
 export class ProUpdateProvider {
+  public message: string
 
-  constructor(public http: HttpClient) {
-    console.log('Hello ProUpdateProvider Provider');
+  public constructor() {}
+
+  async update() {
+    const update = await Pro.deploy.checkForUpdate()
+    if (update.available){
+      this.message = "Actualizacidon aplicación...";
+      await Pro.deploy.downloadUpdate((progress) => {
+        console.log("Descargando actualización: " + progress);
+      })
+      this.message = "Aplicando actualización...";
+      await Pro.deploy.extractUpdate((progress) => {
+        console.log("Aplicando actualización..." + progress);
+      })
+      await Pro.deploy.reloadApp();
+    }
   }
 
 }
