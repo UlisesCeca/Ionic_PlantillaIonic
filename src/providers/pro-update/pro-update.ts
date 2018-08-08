18adda84d5c14093bd2512e59db3c7de63336f85
UlisesCeca <ulises@ulisesceca.com>
2018-08-08 23:44:31 +0200

    prueba ionic pro

--- src/providers/pro-update/pro-update.ts
+++ src/providers/pro-update/pro-update.ts
@@ -15,9 +15,32 @@ export class ProUpdateProvider {
   }
 
   public async update(): Promise<any> {
-    const info = await Pro.deploy.getConfiguration()
-    console.log(info)
+    let nav = this.app.getActiveNav();
+    const update = await Pro.deploy.checkForUpdate();
+
+    if(update.available) {
+      nav.insert(0, PAGES.UPDATE);
+      this.message = "Actualizando aplicación";
+      await Pro.deploy.downloadUpdate((progress) => {
+        this.progress = progress;
+      });
+      this.progress = 0;
+      this.message = "Aplicando actualización";
+      await Pro.deploy.extractUpdate((progress) => {
+        this.progress = progress;
+      })
+      this.progress = 0;
+      this.message = "Actualizando..."
+      await Pro.deploy.reloadApp();
+    } else {
+      nav.insert(0, PAGES.HOME);
+    }
+    //console.log(await this.getConfiguration());
   }
+
+  /*public async getConfiguration(): Promise<ICurrentConfig> {
+    return Pro.deploy.getConfiguration();
+  }*/
 
 
 }
